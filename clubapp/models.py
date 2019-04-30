7d8e5838e13bd9cdc6f13a23b37b7c65537233c4
JILL E NORBERG <35099642+jnorberg1550@users.noreply.github.com>
2019-04-30 10:35:36 -0700

    Add files via upload

--- clubapp/models.py
+++ clubapp/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Meta:
+        db_table='meetings'
+        verbose_name_plural='MeetingTypes'
+
+class MeetingType(models.Model):
+    meetingtitle=models.Charfield (max_length='64')
+    meetingdate=models.Datefield()
+    meetingtime=models=models
+    meetinglocation==models.Charfield (max_length='256')
+    meetingagenda=models.Charfield (max_length='256')
+ 
+ class Meta:
+        db_table= 'minutes'
+        verbose_name_plural='MeetingMinutes'
+
+ class MeetingMinutes(models.Model):
+    minutesid=models.ForeignKey (MeetingType, on_delete models.DO_NOTHING)
+    minutesattendance=models.ManyToManyField(User)
+    minutestext=models.TextField()
+
+    def __str__(self):
+        return self.typename
+    
+class Meta:
+    db_table= 'resource'
+    verbose_name_plural='MeetingResources'
+
+class MeetingResource
+    resourcename=models.Charfield (max_length='64')
+    resourcetype=models.Charfield (max_length='64')
+    resourceurl=models.URLField(null=True, blank=True)
+    resourceentrydate=models.Datefield()
+    resourceuserid
+    resourcedescription=models.TextField()
+
+    def __str__(self):
+        return self.typename
+    
+   class Meta:
+        db_table= 'events'
+        verbose_name_plural='MeetingEvents'
+
+class MeetingEvent
+    eventtitle=models.Charfield (max_length='64')
+    eventlocation=models.Charfield (max_length='64')
+    eventdate=models.Datefield()
+    eventtime=models.
+    eventdescription=models.TextField()
+    eventuserid=models.
+
+    
+
+
+
+
+
+# Register your models here.
+
+
+
+
+
+
+

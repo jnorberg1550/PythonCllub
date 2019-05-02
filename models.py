a421046f7a44bf27ca31ce3766a715de04427f50
JILL E NORBERG <35099642+jnorberg1550@users.noreply.github.com>
2019-05-01 22:35:41 -0700

    Add files via upload

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 from django.contrib.auth.models import User
 
 
@@ -7,28 +8,28 @@ from django.contrib.auth.models import User
 class MeetingType(models.Model):
     meetingtitle=models.Charfield (max_length='64')
     meetingdate=models.Datefield()
-    meetingtime=models=models.Integer(max_length = '4')
-    meetinglocation==models.Charfield (max_length='256')
-    meetingagenda=models.Textfield()
+    meetingtime=models=models.Integer (max_length='4')
+    meetinglocation==models.Charfield (max_length='255')
+    meetingagenda=models.Charfield (max_length='255')
+ 
+    class Meta:
+        db_table='meetings'
+        verbose_name_plural='MeetingTypes'
 
-class Meta:
-    db_table='meetings'
-    verbose_name_plural='MeetingTypes
- 
- 
+    def __str__(self):
+        return self.meetingtitle
 
  class MeetingMinutes(models.Model):
     minutesid=models.ForeignKey (MeetingType, on_delete models.DO_NOTHING)
     minutesattendance=models.ManyToManyField(User)
+    minutestext=models.TextField()
 
-class Meta:
-    db_table= 'minutes'
-    verbose_name_plural='MeetingMinutes'
-
+    class Meta:
+        db_table= 'minutes'
+        verbose_name_plural='MeetingMinutes'
 
     def __str__(self):
-        return self.typename
-    
+        return self.minutesid
 
 
 class MeetingResource
@@ -36,39 +37,33 @@ class MeetingResource
     resourcetype=models.Charfield (max_length='64')
     resourceurl=models.URLField(null=True, blank=True)
     resourceentrydate=models.Datefield()
-    resourceuserid=models.Integer(max_length = '7')
+    resourceuseridmodels.Charfield (User)
     resourcedescription=models.TextField()
 
+    def __str__(self):
+        return self.resourcename
+    
     class Meta:
     db_table= 'resource'
     verbose_name_plural='MeetingResources'
-
-    def __str__(self):
-        return self.typename
-    
+   
 
 class MeetingEvent
     eventtitle=models.Charfield (max_length='64')
     eventlocation=models.Charfield (max_length='64')
     eventdate=models.Datefield()
-    eventtime=models.Integer(max_length = '4')
+    eventtime=models.
     eventdescription=models.TextField()
-    eventuserid=models.ManyToManyField(User)
+    eventuserid=models.Charfield (User)
 
-   class Meta:
+    
+    class Meta:
         db_table= 'events'
-        verbose_name_plural='MeetingEvents' 
-
-def __str__(self):
-        return self.typename
+        verbose_name_plural='MeetingEvents'
 
 
+    def __str__(self):
+        return self.eventtitle
 
 # Register your models here.
-
-
-
-
-
-
 
